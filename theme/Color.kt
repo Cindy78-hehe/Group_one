832a5fbc130db0b9e74f9752ae41f674682be097
OCHOLA PETER <ocholarugby@gmail.com>
2026-04-30 16:31:35 +0300

    theme
    Dictates the overall "look and feel" of an entire application.

---
+++ theme/Color.kt
@@ -0,0 +1,15 @@
+package com.ndejje.nduupdates.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Purple Palette from Color Palette Image
+val PurpleLightest = Color(0xFFF3CCDE)
+val PurpleLight = Color(0xFFD6A8C4)
+val PurpleMedium = Color(0xFFBA88AE)
+val PurpleMain = Color(0xFF9E6899)
+val PurpleDark = Color(0xFF5B3765)
+
+// Functional colors
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val ErrorRed = Color(0xFFB00020)
